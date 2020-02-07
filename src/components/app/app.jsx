e4aef6137a9fe722cc4db3d4fef99798ba5c5b52
Alena Kazarina <alenakazarina1@gmail.com>
2020-02-07 12:28:00 +0300

    added PropTypes typechecking

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Main from '../main/main.jsx';
 
-// eslint-disable-next-line react/prop-types
-const App = ({offersCount}) => {
+const App = ({locations, offers}) => {
   return (
-    <Main offersCount={offersCount} />
+    <Main locations={locations} offers={offers} />
   );
+};
+
+App.propTypes = {
+  locations: PropTypes.arrayOf(PropTypes.string).isRequired,
+  offers: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired
+  })).isRequired
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import nanoid from 'nanoid';
 import App from './components/app/app.jsx';
 
-const offersCount = 100;
+const LOCATIONS = [`Paris`, `Cologne`, `Brussels`, `Amsterdam`, `Hamburg`, `Dusseldorf`];
 
-ReactDOM.render(<App offersCount={offersCount} />, document.querySelector(`#root`));
+const offers = [
+  {
+    id: nanoid(),
+    title: `Beautiful & luxurious apartment at great location`
+  },
+  {
+    id: nanoid(),
+    title: `Wood and stone place`
+  },
+  {
+    id: nanoid(),
+    title: `Canal View Prinsengracht`
+  },
+  {
+    id: nanoid(),
+    title: `Nice, cozy, warm big bed apartment`
+  },
+  {
+    id: nanoid(),
+    title: `Cozy rooms in the city center`
+  }
+];
+
+ReactDOM.render(<App locations={LOCATIONS} offers={offers} />, document.querySelector(`#root`));

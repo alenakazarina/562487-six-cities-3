9270389ea2338609bd43d1d2985662c7e576a8fd
Alena Kazarina <alenakazarina1@gmail.com>
2020-02-13 15:08:41 +0300

    updated with mocks

--- src/index.js
+++ src/index.js
@@ -1,56 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import nanoid from 'nanoid';
 import App from './components/app/app';
+import {offers} from './mocks/offers';
 
-const LOCATIONS = [
-  {
-    title: `Paris`,
-    isActive: true
-  },
-  {
-    title: `Cologne`,
-    isActive: false
-  },
-  {
-    title: `Brussels`,
-    isActive: false
-  },
-  {
-    title: `Amsterdam`,
-    isActive: false
-  },
-  {
-    title: `Hamburg`,
-    isActive: false
-  },
-  {
-    title: `Dusseldorf`,
-    isActive: false
-  }
-];
-
-const OFFERS = [
-  {
-    id: nanoid(),
-    title: `Beautiful & luxurious apartment at great location`
-  },
-  {
-    id: nanoid(),
-    title: `Wood and stone place`
-  },
-  {
-    id: nanoid(),
-    title: `Canal View Prinsengracht`
-  },
-  {
-    id: nanoid(),
-    title: `Nice, cozy, warm big bed apartment`
-  },
-  {
-    id: nanoid(),
-    title: `Cozy rooms in the city center`
-  }
-];
-
-ReactDOM.render(<App locations={LOCATIONS} offers={OFFERS} />, document.querySelector(`#root`));
+ReactDOM.render(<App offers={offers} />, document.querySelector(`#root`));

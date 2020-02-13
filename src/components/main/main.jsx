4e19931d7fab8f92455bf54bc87a10911eb6cc47
Alena Kazarina <alenakazarina1@gmail.com>
2020-02-13 15:07:36 +0300

    updated main with mocks

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Header from '../header/header';
-import LocationTab from '../location-tab/location-tab';
+import Tabs from '../tabs/tabs';
 import Sorting from '../sorting/sorting';
-import PlaceCard from '../place-card/place-card';
+import PlacesList from '../places-list/places-list';
 
-const Main = ({locations, offers, onTitleClick}) => {
-  const activeLocation = locations.find((location) => location.isActive).title;
+const Main = ({city, offers, onTitleClick}) => {
   const offersCount = offers.length;
 
   return offersCount ? (
@@ -14,26 +13,17 @@ const Main = ({locations, offers, onTitleClick}) => {
       <Header />
       <main className="page__main page__main--index">
         <h1 className="visually-hidden">Cities</h1>
-        <div className="tabs">
-          <section className="locations container">
-            <ul className="locations__list tabs__list">
-              {locations.map((location) => (
-                <LocationTab key={location.title} location={location} />
-              ))}
-            </ul>
-          </section>
-        </div>
+        <Tabs activeTab={city} />
         <div className="cities">
           <div className="cities__places-container container">
             <section className="cities__places places">
               <h2 className="visually-hidden">Places</h2>
-              <b className="places__found">{offersCount} places to stay in {activeLocation}</b>
+              <b className="places__found">{offersCount} places to stay in {city}</b>
               <Sorting />
-              <div className="cities__places-list places__list tabs__content">
-                {offers.map((offer) => (
-                  <PlaceCard key={offer.id} title={offer.title} onTitleClick={onTitleClick} />
-                ))}
-              </div>
+              <PlacesList
+                offers={offers}
+                onTitleClick={onTitleClick}
+              />
             </section>
             <div className="cities__right-section">
               <section className="cities__map map"></section>
@@ -47,21 +37,13 @@ const Main = ({locations, offers, onTitleClick}) => {
       <Header />
       <main className="page__main page__main--index page__main--index-empty">
         <h1 className="visually-hidden">Cities</h1>
-        <div className="tabs">
-          <section className="locations container">
-            <ul className="locations__list tabs__list">
-              {locations.map((location) => (
-                <LocationTab key={location.title} location={location} />
-              ))}
-            </ul>
-          </section>
-        </div>
+        <Tabs activeTab={city} />
         <div className="cities">
           <div className="cities__places-container cities__places-container--empty container">
             <section className="cities__no-places">
               <div className="cities__status-wrapper tabs__content">
                 <b className="cities__status">No places to stay available</b>
-                <p className="cities__status-description">We could not find any property availbale at the moment in {activeLocation}</p>
+                <p className="cities__status-description">We could not find any property availbale at the moment in {city}</p>
               </div>
             </section>
             <div className="cities__right-section"></div>
@@ -73,13 +55,16 @@ const Main = ({locations, offers, onTitleClick}) => {
 };
 
 Main.propTypes = {
-  locations: PropTypes.arrayOf(PropTypes.shape({
-    title: PropTypes.string.isRequired,
-    isActive: PropTypes.bool.isRequired
-  })).isRequired,
+  city: PropTypes.string.isRequired,
   offers: PropTypes.arrayOf(PropTypes.shape({
     id: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired
+    previewImage: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    isFavorite: PropTypes.bool.isRequired,
+    isPremium: PropTypes.bool.isRequired,
+    rating: PropTypes.number.isRequired,
+    type: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired
   })).isRequired,
   onTitleClick: PropTypes.func.isRequired
 };
--- src/components/main/main.test.js
+++ src/components/main/main.test.js
@@ -1,28 +1,53 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Main from './main';
-import {locations, offers} from '../../mock/tests';
+
+const offers = [
+  {
+    id: `01`,
+    previewImage: `image`,
+    title: `title`,
+    isFavorite: true,
+    isPremium: false,
+    rating: 2,
+    type: `House`,
+    price: 500
+  },
+  {
+    id: `02`,
+    previewImage: `image`,
+    title: `title`,
+    isFavorite: false,
+    isPremium: true,
+    rating: 5,
+    type: `Hotel`,
+    price: 1000
+  }
+];
 
 const onTitleClick = () => {};
 
-it(`should render Main correctly with no empty offers`, () => {
-  const tree = renderer.create(
-      <Main
-        locations={locations}
-        offers={offers}
-        onTitleClick={onTitleClick}
-      />
-  ).toJSON();
-  expect(tree).toMatchSnapshot();
+describe(`Main`, () => {
+  it(`should render Main correctly with no empty offers`, () => {
+    const tree = renderer.create(
+        <Main
+          city={`Paris`}
+          offers={offers}
+          onTitleClick={onTitleClick}
+        />
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it(`should render Main correctly with empty offers`, () => {
+    const tree = renderer.create(
+        <Main
+          city={`Paris`}
+          offers={[]}
+          onTitleClick={onTitleClick}
+        />
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
 
-it(`should render Main correctly with empty offers`, () => {
-  const tree = renderer.create(
-      <Main
-        locations={locations}
-        offers={[]}
-        onTitleClick={onTitleClick}
-      />
-  ).toJSON();
-  expect(tree).toMatchSnapshot();
-});

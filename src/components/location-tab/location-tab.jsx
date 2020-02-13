2ea2f9d8e92eae434347dc333e935205ae449b8d
Alena Kazarina <alenakazarina1@gmail.com>
2020-02-13 15:06:43 +0300

    updated LocationTab

--- src/components/location-tab/location-tab.jsx
+++ src/components/location-tab/location-tab.jsx
@@ -3,22 +3,19 @@ import PropTypes from 'prop-types';
 
 const ACTIVE_CLASS = `tabs__item--active`;
 
-const LocationTab = ({location}) => {
-  const {title, isActive} = location;
+const LocationTab = ({location, isActive}) => {
   return (
     <li className="locations__item">
       <a className={`locations__item-link tabs__item ${isActive ? ACTIVE_CLASS : ``}`} href="#">
-        <span>{title}</span>
+        <span>{location}</span>
       </a>
     </li>
   );
 };
 
 LocationTab.propTypes = {
-  location: PropTypes.shape({
-    title: PropTypes.string.isRequired,
-    isActive: PropTypes.bool.isRequired
-  })
+  location: PropTypes.string.isRequired,
+  isActive: PropTypes.bool.isRequired
 };
 
 export default LocationTab;
--- src/components/location-tab/location-tab.test.jsx
+++ src/components/location-tab/location-tab.test.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import LocationTab from './location-tab';
-import {locations} from '../../mock/tests';
 
-it(`should render LocationTab correctly`, () => {
-  const tree = renderer.create(
-      <LocationTab location={locations[1]} />
-  ).toJSON();
-  expect(tree).toMatchSnapshot();
-});
+const locations = [`Paris`, `Cologne`, `Brussels`, `Amsterdam`, `Hamburg`, `Dusseldorf`];
 
-it(`should render LocationTab with active class correctly`, () => {
-  const tree = renderer.create(
-      <LocationTab location={locations[0]} />
-  ).toJSON();
-  expect(tree).toMatchSnapshot();
+describe(`LocationTab`, () => {
+  it(`should render LocationTab correctly`, () => {
+    const tree = renderer.create(
+        <LocationTab
+          location={locations[1]}
+          isActive={false}
+        />
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it(`should render LocationTab with active class correctly`, () => {
+    const tree = renderer.create(
+        <LocationTab
+          location={locations[0]}
+          isActive={true}
+        />
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });

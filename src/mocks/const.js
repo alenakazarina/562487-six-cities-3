a577b5aae77e238bf4840afb4a0fb76454c744f3
Alena Kazarina <alenakazarina1@gmail.com>
2020-03-09 12:21:50 +0300

    updated mocks

--- src/mocks/const.js
+++ src/mocks/const.js
@@ -1,171 +1,160 @@
-export const IMAGE_PATH = `./img/`;
-
-export const MAP_ZOOM = 12;
-
-export const MONTHS = [
-  `January`, `February`, `March`, `April`, `May`, `June`, `July`,
-  `August`, `September`, `October`, `November`, `December`
+const titles = [
+  `Amazing and Extremely Central Flat`,
+  `The house among olive`,
+  `Nice, cozy, warm big bed apartment`
 ];
 
-const OfferTypes = {
-  apartment: `Apartment`,
-  room: `Private Room`,
-  house: `House`,
-  hotel: `Hotel`
-};
+const galleryImages = [`image1`, `image2`, `image3`, `image4`, `image5`, `image6`, `image7`];
 
-export const OffersData = {
-  maxRating: 5,
-  types: Object.values(OfferTypes),
-  titles: [
-    `Canal View room`,
-    `Nice, cozy, warm big bed apartment`,
-    `Wood and stone place`,
-    `Beautiful & luxurious apartment at great location`,
-    `Cozy rooms in the city center`
-  ],
-  images: [
-    `apartment-01.jpg`,
-    `apartment-02.jpg`,
-    `apartment-03.jpg`,
-    `apartment-04.jpg`,
-    `apartment-05.jpg`,
-  ],
-  amenities: [`Wi-Fi`, `Washing machine`, `Towels`, `Heating`, `Coffee machine`, `Baby seat`, `Kitchen`, `Cabel TV`, `Fridge`],
-  description: `An independent House, strategically located between Rembrand Square and National Opera, but where the bustle of the city comes to rest in this alley flowery and colorful.`
-};
+export const cities = [`Paris`, `Cologne`, `Brussels`, `Amsterdam`, `Hamburg`, `Dusseldorf`];
 
-export const UserData = {
-  names: [`Douglas`, `James`, `Matthew`, `John`, `Leo`, `Robert`, `Dirk`],
-  avatarUrls: [
-    `user-1.jpg`,
-    `user-2.jpg`,
-    `user-3.jpg`,
-    `user-4.jpg`,
-    `user-5.jpg`,
-  ],
-  comment: `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.`
-};
-
-export const Cities = {
-  PARIS: {
+const locations = [
+  {
     name: `Paris`,
-    cityCenter: {
-      latitude: 48.859335,
-      longitude: 2.350730
-    },
-    offersLocations: [{
-      latitude: 48.872344,
-      longitude: 2.360418
-    }, {
-      latitude: 48.866015,
-      longitude: 2.346708
-    }, {
-      latitude: 48.850586,
-      longitude: 2.327006
-    }, {
-      latitude: 48.855066,
-      longitude: 2.316033
-    }]
+    location: {
+      latitude: 1,
+      longitude: 1,
+      zoom: 13
+    }
   },
-  COLOGNE: {
-    name: `Cologne`,
-    cityCenter: {
-      latitude: 50.939485,
-      longitude: 6.948775
-    },
-    offersLocations: [{
-      latitude: 50.937764,
-      longitude: 6.961390
-    }, {
-      latitude: 50.945324,
-      longitude: 6.960608
-    }, {
-      latitude: 50.933751,
-      longitude: 6.936701
-    }, {
-      latitude: 50.931175,
-      longitude: 6.948593
-    }]
-  },
-  BRUSSELS: {
-    name: `Brussels`,
-    cityCenter: {
-      latitude: 50.855889,
-      longitude: 4.351608
-    },
-    offersLocations: [{
-      latitude: 50.854885,
-      longitude: 4.345541
-    }, {
-      latitude: 50.858440,
-      longitude: 4.329629
-    }, {
-      latitude: 50.835941,
-      longitude: 4.346313
-    }, {
-      latitude: 50.834979,
-      longitude: 4.351570
-    }]
-  },
-  AMSTERDAM: {
-    name: `Amsterdam`,
-    cityCenter: {
-      latitude: 52.371512,
-      longitude: 4.893750
-    },
-    offersLocations: [{
-      latitude: 52.370955,
-      longitude: 4.873096
-    }, {
-      latitude: 52.379553,
-      longitude: 4.873096
-    }, {
-      latitude: 52.390955,
-      longitude: 4.909309
-    }, {
-      latitude: 52.360955,
-      longitude: 4.909309
-    }]
-  },
-  HAMBURG: {
-    name: `Hamburg`,
-    cityCenter: {
-      latitude: 53.556238,
-      longitude: 9.985139
-    },
-    offersLocations: [{
-      latitude: 53.553348,
-      longitude: 9.971704
-    }, {
-      latitude: 53.552588,
-      longitude: 10.019478
-    }, {
-      latitude: 53.561941,
-      longitude: 10.018891
-    }, {
-      latitude: 53.548758,
-      longitude: 9.978013
-    }]
-  },
-  DUSSELDORF: {
+  {
     name: `Dusseldorf`,
-    cityCenter: {
-      latitude: 51.225322,
-      longitude: 6.774754
-    },
-    offersLocations: [{
-      latitude: 51.219590,
-      longitude: 6.774384
-    }, {
-      latitude: 51.232822,
-      longitude: 6.780301
-    }, {
-      latitude: 51.220024,
-      longitude: 6.792541
-    }, {
-      latitude: 51.215356,
-      longitude: 6.781649
-    }]
+    location: {
+      latitude: 2,
+      longitude: 2,
+      zoom: 13
+    }
   }
-};
+];
+
+const offerTypes = [`apartment`, `room`, `house`, `hotel`];
+
+const goods = [`Breakfast`, `Laptop friendly workspace`, `Washer`];
+
+const sentences = [
+  `Relax, rejuvenate and unplug in this ultimate rustic getaway experience in the country.`,
+  `In our beautiful screened Pondhouse, you can gaze at the stars and listen to the sounds of nature from your cozy warm bed.`
+];
+
+export const users = [
+  {
+    avatarUrl: `/user1_avatar`,
+    id: 1,
+    isPro: true,
+    name: `Alena`
+  },
+  {
+    avatarUrl: `/user2_avatar`,
+    id: 1,
+    isPro: true,
+    name: `Nick`
+  }
+];
+
+export const appUsers = users.map((user) => Object.assign({}, user, {email: `user@gmail.com`}));
+
+export const reviews = [{
+  id: 1,
+  user: users[0],
+  rating: 4,
+  comment: sentences[0],
+  date: new Date(`2020.03.04`)
+}, {
+  id: 2,
+  user: users[1],
+  rating: 5,
+  comment: sentences[1],
+  date: new Date(`2020.03.03`)
+}];
+
+export const cityOffers = [{
+  id: 1,
+  city: locations[0],
+  previewImage: galleryImages[0],
+  images: galleryImages,
+  title: titles[0],
+  isFavorite: true,
+  isPremium: true,
+  rating: 5,
+  type: offerTypes[0],
+  bedrooms: 3,
+  maxAdults: 5,
+  price: 400,
+  amenities: goods,
+  host: users[0],
+  description: sentences[0],
+  location: locations[0].location
+}, {
+  id: 2,
+  city: locations[1],
+  previewImage: galleryImages[2],
+  images: galleryImages,
+  title: titles[1],
+  isFavorite: false,
+  isPremium: false,
+  rating: 4,
+  type: offerTypes[2],
+  bedrooms: 1,
+  maxAdults: 7,
+  price: 500,
+  amenities: goods,
+  host: users[1],
+  description: sentences[1],
+  location: locations[1].location
+}, {
+  id: 3,
+  city: locations[1],
+  previewImage: galleryImages[2],
+  images: galleryImages,
+  title: titles[0],
+  isFavorite: false,
+  isPremium: true,
+  rating: 3,
+  type: offerTypes[1],
+  bedrooms: 2,
+  maxAdults: 2,
+  price: 600,
+  amenities: goods,
+  host: users[0],
+  description: sentences[0],
+  location: locations[1].location
+}];
+
+export const favoriteOffers = cityOffers.map((offer) => Object.assign({}, offer, {isFavorite: true}));
+
+export const apiMockOffers = [{
+  'bedrooms': 3,
+  'city': `Paris`,
+  'description': sentences[1],
+  'goods': goods,
+  'host': {
+    'avatar_url': `avatar`,
+    'id': 1,
+    'is_pro': true,
+    'name': `Alena`
+  },
+  'id': 1,
+  'images': galleryImages,
+  'is_favorite': true,
+  'is_premium': true,
+  'location': locations[1],
+  'max_adults': 3,
+  'preview_image': galleryImages[0],
+  'price': 600,
+  'rating': 3,
+  'title': titles[1],
+  'type': offerTypes[1]
+}];
+
+export const apiMockReviews = [{
+  'comment': sentences[1],
+  'date': new Date(`2020.03.03`).toISOString(),
+  'id': 1,
+  'rating': 3.5,
+  'user': {
+    'avatar_url': `userAvatar`,
+    'id': 1,
+    'is_pro': true,
+    'name': `Alena`
+  }
+}];
--- src/mocks/tests.js
+++ src/mocks/tests.js
@@ -1,144 +1,73 @@
-export const cities = [`Paris`, `Cologne`, `Brussels`, `Amsterdam`, `Hamburg`, `Dusseldorf`];
+import configureStore from 'redux-mock-store';
+import {AuthStatus} from '../reducers/user/user';
+import {cityOffers, reviews, appUsers} from './const';
 
-export const cityOffers = [{
-  id: `01`,
-  previewImage: `image`,
-  title: `title`,
-  images: [`image`, `image`],
-  isFavorite: true,
-  isPremium: false,
-  rating: 3,
-  features: {
-    type: `House`,
-    bedrooms: 3,
-    maxAdults: 6
-  },
-  price: 200,
-  amenities: [`amenity`, `amenity`],
-  host: {
-    id: `06`,
-    name: `James`,
-    isPro: true,
-    avatarUrl: `avatar`
-  },
-  description: `House description`,
-  reviews: [{
-    id: `110`,
-    user: {
-      id: `10`,
-      name: `Bob`,
-      isPro: false,
-      avatarUrl: `avatar`
-    },
-    rating: 4,
-    comment: `comment`,
-    date: new Date(`2019-04-24`)
-  }],
-  city: {
-    name: `Amsterdam`,
-    location: {
-      latitude: 53.552645,
-      longitude: 9.966287
-    }
-  }
-},
-{
-  id: `02`,
-  previewImage: `image`,
-  title: `title`,
-  images: [`image`, `image`],
-  isFavorite: true,
-  isPremium: false,
-  rating: 4,
-  features: {
-    type: `Hotel`,
-    bedrooms: 3,
-    maxAdults: 6
-  },
-  price: 500,
-  amenities: [`amenity`, `amenity`],
-  host: {
-    id: `03`,
-    name: `Bob`,
-    isPro: true,
-    avatarUrl: `avatar`
-  },
-  description: `Hotel description`,
-  reviews: [{
-    id: `05`,
-    user: {
-      id: `111`,
-      name: `Bob`,
-      isPro: false,
-      avatarUrl: `avatar`
-    },
-    rating: 4,
-    comment: `comment`,
-    date: new Date(`2019-04-24`)
-  }],
-  city: {
-    name: `Amsterdam`,
-    location: {
-      latitude: 53.552645,
-      longitude: 9.966287
-    }
-  }
-}];
-
-export const locations = [
-  {
-    name: `Location1`,
-    location: {
-      latitude: 53.552645,
-      longitude: 9.966287
-    }
-  },
-  {
-    name: `Location2`,
-    location: {
-      latitude: 53.552645,
-      longitude: 9.966287
-    }
-  },
-];
-
-export const nearOffers = cityOffers.map((offer, i) =>
-  Object.assign({}, offer, {id: `${i}`}));
-
-export const users = [{
-  id: `01`,
-  name: `James`,
-  isPro: true,
-  avatarUrl: `avatar`
-},
-{
-  id: `02`,
-  name: `Bob`,
+const defaultUser = {
+  id: -1,
+  email: ``,
+  name: ``,
   isPro: false,
-  avatarUrl: `avatar`
-}];
+  avatarUrl: ``
+};
 
-export const reviews = [{
-  id: `01`,
-  user: {
-    id: `10`,
-    name: `Bob`,
-    isPro: false,
-    avatarUrl: `avatar`
+const mockStore = configureStore([]);
+
+export const storeWithAuth = mockStore({
+  USER: {
+    authStatus: AuthStatus.AUTH,
+    user: appUsers[0]
   },
-  rating: 4,
-  comment: `comment`,
-  date: new Date(`2019-04-24`)
-},
-{
-  id: `02`,
-  user: {
-    id: `20`,
-    name: `Jane`,
-    isPro: false,
-    avatarUrl: `avatar`
+  OFFERS: {
+    offers: cityOffers,
+    activeCity: `Paris`
   },
-  rating: 5,
-  comment: `comment`,
-  date: new Date(`2019-04-24`)
-}];
+  OFFER: {
+    pageOffer: cityOffers[1],
+    activeOffer: cityOffers[2],
+    nearOffers: cityOffers.slice(0, 2),
+    comments: reviews
+  },
+  FAVORITES: {
+    favorites: cityOffers
+  }
+});
+
+export const storeWithNoAuth = mockStore({
+  USER: {
+    authStatus: AuthStatus.NO_AUTH,
+    user: defaultUser
+  },
+  OFFERS: {
+    offers: cityOffers,
+    activeCity: `Paris`
+  },
+  OFFER: {
+    pageOffer: null,
+    activeOffer: null,
+    nearOffers: [],
+    comments: []
+  },
+  FAVORITES: {
+    favorites: []
+  }
+});
+
+export const storeWithNoOffers = mockStore({
+  USER: {
+    authStatus: AuthStatus.NO_AUTH,
+    user: defaultUser
+  },
+  OFFERS: {
+    offers: [],
+    activeCity: `Paris`
+  },
+  OFFER: {
+    pageOffer: null,
+    activeOffer: null,
+    nearOffers: [],
+    comments: []
+  },
+  FAVORITES: {
+    favorites: []
+  }
+});
